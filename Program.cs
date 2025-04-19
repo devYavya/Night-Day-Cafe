2a1cf358e92e81b9d28db885faed1780ede35d07
devYavya <yavya.sharma21@gmail.com>
2025-04-19 16:54:15 +0530

    dockerfile readded

--- Program.cs
+++ Program.cs
@@ -11,10 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowFrontend",
-        builder => builder.WithOrigins("http://localhost:3000")
-                         .AllowAnyMethod()
-                         .AllowAnyHeader());
+    options.AddPolicy("AllowFrontend", policy =>
+        policy.WithOrigins("https://night-day-cafe-face.onrender.com")
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials());
 });
 
 builder.Services.Configure<MongoDbSettings>(
@@ -26,35 +27,33 @@ builder.Services.AddSingleton<MongoDbContext>(sp =>
     return new MongoDbContext(Options.Create(settings));
 });
 
-// Register repositories
 builder.Services.AddSingleton<MenuRepository>();
 builder.Services.AddScoped<UserRepository>();
 builder.Services.AddScoped<BillingRepository>();
 builder.Services.AddScoped<InventoryRepository>();
 
-// Add controllers
 builder.Services.AddControllers()
     .AddNewtonsoftJson(options => options.UseMemberCasing());
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Use CORS
+app.UseHttpsRedirection();
+app.UseRouting();
 app.UseCors("AllowFrontend");
+app.UseAuthorization();
+app.MapControllers();
 
-// Load menu items from JSON into MongoDB
 using (var scope = app.Services.CreateScope())
 {
     var menuRepo = scope.ServiceProvider.GetRequiredService<MenuRepository>();
     await LoadMenuData(menuRepo);
 }
 
-app.UseAuthorization();
-app.MapControllers();
 app.Run();
 
-// Method to load menu data from JSON file
 static async Task LoadMenuData(MenuRepository menuRepo)
 {
     string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "menu.json");
@@ -62,16 +61,12 @@ static async Task LoadMenuData(MenuRepository menuRepo)
     if (File.Exists(filePath))
     {
         var jsonData = await File.ReadAllTextAsync(filePath);
-        var menuItems = Newtonsoft.Json.JsonConvert.DeserializeObject<List<MenuItem>>(jsonData);
+        var menuItems = JsonConvert.DeserializeObject<List<MenuItem>>(jsonData);
 
-        if (menuItems != null)
+        if (menuItems != null && !await menuRepo.HasData())
         {
-            // Insert only if the collection is empty
-            if (!await menuRepo.HasData())
-            {
-                await menuRepo.AddMultipleMenuItems(menuItems);
-                Console.WriteLine("Menu items loaded from JSON file into MongoDB.");
-            }
+            await menuRepo.AddMultipleMenuItems(menuItems);
+            Console.WriteLine("Menu items loaded from JSON file into MongoDB.");
         }
     }
 }
